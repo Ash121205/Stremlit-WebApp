f1f7374b8ca60e4cb52303a09ebade724ec5974f
Ash121205 <aasheshkaran20@gmail.com>
2023-04-17 14:08:05 +0530

    Changed the title

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
     layout='wide',
     initial_sidebar_state='expanded'
     )
-st.title("NLP")
+st.title("NLP Master: Unlocking the Secrets of Language")
 
 nav = st.sidebar.selectbox('Navigation',['Lemmatizer', 'POS_Tagging', 'Noun Phrase Chunking'])
 
